97992b0f2796caf202c174668af005e067233ec6
John <john@johns-mbp.home>
2023-03-11 15:06:29 +0000

    Initialised project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Test
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
